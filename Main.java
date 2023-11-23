5f14cd7f5eec99fccba74dc273bcb7d20c077a57
karlaribeirodev <150819095+karlaribeirodev@users.noreply.github.com>
2023-11-23 17:22:07 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[] args) {
+
+      String valor = "Descomplica-Java";
+       System.out.println(valor.contains( "Java") ); 
+       System.out.println(valor.contains( "Descomplica"));
+       System.out.println(valor.contains( "hello") );
+    
+
+
+    
+  }
+}
